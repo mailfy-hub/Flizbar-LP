2d67d4ef6343791d51af759a80ae5efd253d56ec
Igor Mazetti <iigormazetti@hotmail.com>
2024-05-20 09:27:03 -0300

    feat: added home page languages and styles

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,63 +1,33 @@
-'use client'
+"use client";
 import { Locale } from "@/i18n.config";
 import bg from "../../assets/background_hero.png";
-import Script from "next/script";
 import Header from "@/components/Header/Header";
 import MainContentRouter from "@/components/MainContentRouter/MainContentRouter";
 import { useDictionary } from "./dictionary-provider";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Page({ params: { lang } }: { params: { lang: Locale } }) {
-  const dictionary = useDictionary()
-  const [contentIndex, setContentIndex] = useState("initial")
+  const dictionary = useDictionary();
+  const [contentIndex, setContentIndex] = useState("init");
+
+  const handleContentIndex = (content: string) => {
+    setContentIndex(content);
+    localStorage.setItem("contentIndex", content);
+  };
+
+  const savedContent = localStorage.getItem("contentIndex");
+  useEffect(() => {
+    if (savedContent) {
+      setContentIndex(savedContent);
+    }
+  }, [savedContent]);
 
   return (
     <>
-      <div className="px-[112px] py-0 bg-cover bg-center h-[calc(100vh-80px)]" style={{ backgroundImage: `url(${bg.src})` }}>
-        <Header lang={lang} languages={dictionary.header} btn={dictionary.languages} handleContentIndex={setContentIndex} />        
-        <MainContentRouter languages={dictionary.page} contentIndex={contentIndex} />
+      <div className="px-[20px] md:px-[60px] 2xl:px-[112px] py-0 bg-cover bg-center" style={{ backgroundImage: `url(${bg.src})` }}>
+        <Header lang={lang} languages={dictionary.header} btn={dictionary.languages} handleContentIndex={handleContentIndex} />
+        <MainContentRouter languages={dictionary} contentIndex={contentIndex} />
       </div>
-      <div className="tradingview-widget-container" style={{ marginBottom: "0px" }}>
-        <div className="tradingview-widget-container__widget"></div>
-        <div className="tradingview-widget-copyright"></div>
-      </div>
-      <Script
-        id="my-script-tradingview"
-        src="https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js"
-        strategy="afterInteractive"
-      >
-        {`
-          {
-            "symbols": [
-              {
-                "proName": "FOREXCOM:SPXUSD",
-                "title": "S&P 500 Index"
-              },
-              {
-                "proName": "FOREXCOM:NSXUSD",
-                "title": "US 100 Cash CFD"
-              },
-              {
-                "proName": "FX_IDC:EURUSD",
-                "title": "EUR to USD"
-              },
-              {
-                "proName": "BITSTAMP:BTCUSD",
-                "title": "Bitcoin"
-              },
-              {
-                "proName": "BITSTAMP:ETHUSD",
-                "title": "Ethereum"
-              }
-            ],
-            "showSymbolLogo": true,
-            "isTransparent": false,
-            "displayMode": "adaptive",
-            "colorTheme": "dark",
-            "locale": "en"
-          }
-        `}
-      </Script>
     </>
   );
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,13 +4,9 @@ import LocaleSwitch from "@/components/LocaleSwitch/LocaleSwitch";
 import logo from "../../assets/logo.png";
 import vector from "../../assets/Vector.png";
 import Image from "next/image";
-
-interface HeaderProps {
-  lang: Locale;
-  languages: any;
-  btn: any;
-  handleContentIndex: (index: string) => void;
-}
+import OpenAccButton from "../OpenAccButton/OpenAccButton";
+import { useState } from "react";
+import { List, X } from "phosphor-react";
 
 export default function Header({
   lang,
@@ -23,34 +19,97 @@ export default function Header({
   btn: any;
   handleContentIndex: (index: string) => void;
 }) {
+  const [open, setOpen] = useState(false);
+
+  const handleMenu = () => {
+    setOpen((prev) => !prev);
+  };
+
   return (
-    <header className="py-6 bg-transparent">
-      <nav className="container flex items-center justify-between">
-        <div className="flex items-center gap-x-[48px]">
+    <header className="py-6 bg-transparent w-full">
+      <nav className="container flex items-center justify-between w-full">
+        <div className="flex items-center gap-x-[48px] justify-between">
           <div className="w-[125px] h-[32px]" style={{ backgroundImage: `url(${logo.src})` }}></div>
 
-          <div className="flex items-center gap-x-[24px] mt-2">
-            <button onClick={() => handleContentIndex("initial")}>{languages.beginning}</button>
+          <div className="hidden md:flex items-center gap-x-[24px] mt-2">
+            <button onClick={() => handleContentIndex("init")}>{languages.beginning}</button>
             <button onClick={() => handleContentIndex("advantages")}>{languages.advantages}</button>
             <button onClick={() => handleContentIndex("know")}>{languages.getToKnow}</button>
             <button onClick={() => handleContentIndex("plans")}>{languages.plans}</button>
+            <button onClick={() => handleContentIndex("contact")}>{languages.contact}</button>
           </div>
         </div>
 
-        <div className="flex items-center gap-[16px] -mb-2">
+        <div className="hidden md:flex items-center gap-[16px] -mb-2">
           <LocaleSwitch lang={lang} languages={btn} />
           <button className="flex gap-[8px] border w-[154px] h-[40px] items-center justify-center rounded font-semibold text-[16px]">
             {languages.client} <Image alt="user-img" src={vector.src} width={20} height={20} />
           </button>
+          <OpenAccButton text={languages.buttonTxt} />
+        </div>
 
-          <button className="flex items-center gap-2 bg-[#A06A08] h-[40px] text-white py-2 px-4 rounded font-bold text-[16px]">
-            {languages.buttonTxt}
-            <svg className="fill-current h-6 w-6 transform -rotate-90 stroke-1" viewBox="0 0 20 20">
-              <path d="M10 12l-5-5 1.41-1.41L10 9.17l3.59-3.58L15 7z"></path>
-            </svg>
+        <div className="flex md:hidden">
+          <button type="button" onClick={handleMenu} className="inline-flex intems-center justify-center p-2 rounded-md">
+            <span className="sr-only">Open Menu</span>
+            {open ? <X size={32} /> : <List size={32} />}
           </button>
         </div>
       </nav>
+      {open ? (
+        <div className="md:hidden relative">
+          <div className="flex flex-col items-center gap-2 bg-[#0C0B0A] ox-2 pt-2 pb-3 space-y-1 sm:px-3 absolute top-0 right-1 w-full">
+            <button
+              onClick={() => {
+                handleContentIndex("init");
+                handleMenu();
+              }}
+            >
+              {languages.beginning}
+            </button>
+            <button
+              onClick={() => {
+                handleContentIndex("advantages");
+                handleMenu();
+              }}
+            >
+              {languages.advantages}
+            </button>
+            <button
+              onClick={() => {
+                handleContentIndex("know");
+                handleMenu();
+              }}
+            >
+              {languages.getToKnow}
+            </button>
+            <button
+              onClick={() => {
+                handleContentIndex("plans");
+                handleMenu();
+              }}
+            >
+              {languages.plans}
+            </button>
+
+            <button
+              onClick={() => {
+                handleContentIndex("contact");
+                handleMenu();
+              }}
+            >
+              {languages.contact}
+            </button>
+
+            <LocaleSwitch lang={lang} languages={btn} />
+            <button className="flex gap-[8px] border w-[140px] h-[48px] items-center justify-center rounded font-semibold text-[16px]">
+              {languages.client} <Image alt="user-img" src={vector.src} width={20} height={20} />
+            </button>
+            <div>
+              <OpenAccButton text={languages.buttonTxt} />
+            </div>
+          </div>
+        </div>
+      ) : null}
     </header>
   );
 }
