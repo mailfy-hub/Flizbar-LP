cb24a2f24ba21a4acc2ba397bd7853b5c61c5930
Olávio Timóteo <olaviotimoteo@hotmail.com>
2024-07-11 11:18:50 -0300

    fix: Adjusts error in language file

--- lib/dictionary.ts
+++ lib/dictionary.ts
@@ -4,7 +4,7 @@ import type { Locale } from "@/i18n.config";
 const dictionaries = {
   "pt-BR": () => import("@/dictionaries/pt.json").then((module) => module.default),
   "es-ES": () => import("@/dictionaries/es.json").then((module) => module.default),
-  en: () => import("@/dictionaries/en.json").then((module) => module.default),
+  "en-EN": () => import("@/dictionaries/en.json").then((module) => module.default),
 };
 
 //@ts-ignore
