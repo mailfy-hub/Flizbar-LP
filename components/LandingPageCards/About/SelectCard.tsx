3817bba675e2e1b2b70aabc4e5efd0861ff43b94
Olávio Timóteo <olaviotimoteo@hotmail.com>
2024-07-14 20:36:16 -0300

    chore: Adjusts the service session header

--- components/LandingPageCards/About/SelectCard.tsx
+++ components/LandingPageCards/About/SelectCard.tsx
@@ -16,19 +16,23 @@ export default function AboutCard({
   return (
     <div
       onClick={onClick}
-      className={`flex justify-between items-center h-full cursor-pointer px-[26px] lg:w-full border-b min-w-[300px] ${
+      className={`flex justify-between items-center h-full cursor-pointer px-[26px] lg:w-full border-b min-w-[200px] bg-[#161412] mr-5 ${
         selected
           ? "border-b-[#C89305]"
-          : "border-b-[#7E7873] transition-all hover:opacity-75"
+          : "border-b-[#7e7373] transition-all hover:opacity-75"
       }`}
     >
-      <p className="flex flex-col max-w-[162px] text-[20px]">
+      <p
+        className={`flex flex-col max-w-[162px] text-[20px] ${
+          selected ? "text-[#C89305]" : "text-white"
+        }`}
+      >
         {title}
         {/* <p className={`${selected ? "text-[#C89305]" : "text-white"}`}>
           {subtitle}
         </p> */}
       </p>
-      <ArrowRight />
+      {/* <ArrowRight /> */}
     </div>
   );
 }
