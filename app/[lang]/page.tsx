f029c8630f52b3f0bf95b1089dbe05c975b21801
Olávio Timóteo <olaviotimoteo@hotmail.com>
2024-07-14 20:34:43 -0300

    chore: Remove the Footer component being called inside the ContactCard and implement it inside the main page

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -12,6 +12,7 @@ import { useDictionary } from "./dictionary-provider";
 import AboutCard from "@/components/LandingPageCards/About/AboutCard";
 import Journey from "@/components/LandingPageCards/Journey/Journey";
 import ServicesCard from "@/components/LandingPageCards/ServicesCard/ServicesCard";
+import Footer from "@/components/LandingPageCards/Footer/Footer";
 
 export default function Page({
   params: { lang },
@@ -46,7 +47,7 @@ export default function Page({
         </div>
       </div>
 
-      <div className="flex flex-col mt-[124px] py-[72px] bg-[#1E1C1B] ">
+      <div className="flex flex-col mt-[124px] py-[72px] bg-[#1E1C1B]">
         <div id="about">
           <div className="w-full max-w-[1216px] mx-auto">
             <AboutCard languages={dictionary} />
@@ -54,7 +55,7 @@ export default function Page({
         </div>
       </div>
 
-      <div className="flex flex-col mt-[124px] py-[72px] bg-black ">
+      <div className="flex flex-col mt-[124px] py-[72px] bg-black">
         <div id="journey">
           <div className="w-full max-w-[1216px] mx-auto">
             <Journey languages={dictionary} />
@@ -62,7 +63,7 @@ export default function Page({
         </div>
       </div>
 
-      <div className="flex flex-col mt-[124px] py-[72px] bg-[#1E1C1B] ">
+      <div className="flex flex-col mt-[124px] py-[50px] bg-black">
         <div id="services">
           <div className="w-full max-w-[1200px] mx-auto">
             <ServicesCard languages={dictionary} />
@@ -70,10 +71,18 @@ export default function Page({
         </div>
       </div>
 
-      <div className="flex flex-col mt-[124px] py-[72px] bg-[#1E1C1B] ">
+      <div className="flex flex-col mt-[124px] py-[72px] bg-black">
         <div id="contact">
           <div className="w-full max-w-[1200px] mx-auto">
             <ContactCard languages={dictionary} />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col mt-[124px] py-[72px] bg-black">
+        <div id="footer">
+          <div className="w-full max-w-[1200px] mx-auto">
+            <Footer languages={dictionary} />
           </div>
         </div>
       </div>
--- components/LandingPageCards/ContactCard/ContactCard.tsx
+++ components/LandingPageCards/ContactCard/ContactCard.tsx
@@ -39,12 +39,21 @@ export default function ContactCard({ languages }: { languages: any }) {
   return (
     <div className="flex flex-col w-full 2xl:mt-[180px] 2xl:h-[calc(100vh-165px)] justify-center items-center">
       <div className="flex flex-col justify-center items-center w-full mb-14">
-        <p className="text-[#C89305] font-bold text-[14px] mt-4">{contact.topText}</p>
+        <p className="text-[#C89305] font-bold text-[14px] mt-4">
+          {contact.topText}
+        </p>
         <p className="text-[30px] md:text-[40px] mb-[20px]">{contact.title}</p>
 
-        <p className={`max-w-[510px] text-[16px] mb-[64px] text-center ${inter.className}`}>{contact.subtitle}</p>
+        <p
+          className={`max-w-[510px] text-[16px] mb-[64px] text-center ${inter.className}`}
+        >
+          {contact.subtitle}
+        </p>
 
-        <form onSubmit={handleSubmit(onSubmitHandler)} className="flex flex-col gap-4 max-w-[488px] w-full">
+        <form
+          onSubmit={handleSubmit(onSubmitHandler)}
+          className="flex flex-col gap-4 max-w-[488px] w-full"
+        >
           <input
             className="w-full bg-[#151412] border border-[#302F2D] rounded-sm py-[12px] px-[15px] pt-4 "
             {...register("name")}
@@ -69,12 +78,15 @@ export default function ContactCard({ languages }: { languages: any }) {
             required
           />
 
-          <button type="submit" className="w-full h-[48px] bg-[#A06A08] font-bold">
+          <button
+            type="submit"
+            className="w-full h-[48px] bg-[#A06A08] font-bold"
+          >
             {contact.send}
           </button>
         </form>
       </div>
-      <Footer languages={languages} />
+      {/* <Footer languages={languages} /> */}
     </div>
   );
 }
