4c4e83557167fbaf480b99aed83725643ddf6821
Olávio Timóteo <olaviotimoteo@hotmail.com>
2024-07-16 16:38:48 -0300

    feat: Insert the link to redirect to login in the home block

--- components/LandingPageCards/BegginPresentationCard/BegginPresentationCard.tsx
+++ components/LandingPageCards/BegginPresentationCard/BegginPresentationCard.tsx
@@ -4,7 +4,11 @@ import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function BegginPresentationCard({ languages }: { languages: any }) {
+export default function BegginPresentationCard({
+  languages,
+}: {
+  languages: any;
+}) {
   return (
     <div className="flex flex-col relative w-full h-[calc(100vh-165px)] justify-end md:mt-[0px] px-[20px] md:px-[0px]">
       <div className="w-full max-w-[727px]">
@@ -14,13 +18,24 @@ export default function BegginPresentationCard({ languages }: { languages: any }
         <p className="max-w-[591px] text-[32px] leading-[35px] md:text-[48px] font-semibold mb-[24px] md:leading-[55px]">
           {languages.page.default.beginning.title}
         </p>
-        <p className={`text-[16px] md:text-[20px] mb-[64px] ${inter.className}`}>{languages.page.default.beginning.text}</p>
+        <p
+          className={`text-[16px] md:text-[20px] mb-[64px] ${inter.className}`}
+        >
+          {languages.page.default.beginning.text}
+        </p>
         <div className="flex flex-col md:flex-row gap-8 2xl:mb-[160px] w-full md:mb-[60px]">
-          <OpenAccButton className="max-w-[232px] md:max-w-full h-[46px]" text={languages.page.default.beginning.button} />
+          <a href="https://dashboard.flizbar.com/login/">
+            <OpenAccButton
+              className="max-w-[232px] md:max-w-full h-[46px]"
+              text={languages.page.default.beginning.button}
+            />
+          </a>
 
           <p className="text-[25px] md:text-[14px] md:max-w-[221px] text-center md:text-start w-full md:mb-0 mb-8">
             {languages.page.default.beginning.presentation}
-            <span className="text-[#C89305] ml-[5px] font-bold">{languages.page.default.beginning.presentationSpan}</span>
+            <span className="text-[#C89305] ml-[5px] font-bold">
+              {languages.page.default.beginning.presentationSpan}
+            </span>
           </p>
         </div>
       </div>
